1ff049c37d1c83fd733877513f9c5f5ba1bacb9c
Rehan959 <rehanshami007@gmail.com>
2026-06-28 06:28:08 +0530

    added the delpoyment setup and health check

--- app.js
+++ app.js
@@ -6,6 +6,13 @@ const PORT = process.env.PORT || 5001
 
 app.use(express.static('dist'))
 
+app.get('/health', (res) => {
+  // eslint-disable-next-line no-constant-condition
+  if (true) throw ('error...  ')
+  res.send('ok')
+})
+
+
 const start = async () => {
   await app.listen(PORT)
   console.log(`server started on port ${PORT}`)
--- tests/example.spec.js
+++
@@ -1,9 +0,0 @@
-const { test, describe, expect, beforeEach } = require('@playwright/test')
-
-describe('Pokedex', () => {
-  test('front page can be opened', async ({ page }) => {
-    await page.goto('http://localhost:8080/')
-    await expect(page.getByText('Ivysaur')).toBeVisible()
-    await expect(page.getByText('Pokémon and Pokémon character names are trademarks of Nintendo.')).toBeVisible()
-  })
-})
